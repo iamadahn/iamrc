bd4d17e8886969ef555278b629d49b9b5890316c
iamadahn <vendonism16@yandex.ru>
2024-05-30 22:07:57 +0300

    added led driver

--- app/led_controller/led_controller.c
+++ app/led_controller/led_controller.c
@@ -1,13 +1,19 @@
 #include "led_controller.h"
+#include "led.h"
 #include "FreeRTOS.h"
 #include "FreeRTOSConfig.h"
 #include "task.h"
-#include "stm32f1xx_ll_gpio.h"
 
 void
 led_controller_handler(void) {
+    led_t led_debug = {
+        .port = GPIOB,
+        .pin = LL_GPIO_PIN_2,
+        .state = 0,
+    };
+
     while (1) {
-        LL_GPIO_TogglePin(GPIOB, LL_GPIO_PIN_2);
+        led_toggle(&led_debug);
         vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
