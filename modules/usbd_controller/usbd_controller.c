6e1d50cd515d03b4a6225cbe8d1aa038c9206d14
iamadahn <vendonism16@yandex.ru>
2024-08-15 21:21:44 +0300

    usb: connected log_write_ll to usb cdc, added usbd irq handler

--- modules/usbd_controller/usbd_controller.c
+++ modules/usbd_controller/usbd_controller.c
@@ -1,8 +1,8 @@
 #include "usbd_controller.h"
 #include "tusb.h"
 #include "rtos.h"
+#include "string.h"
 
-static void echo_serial_port(uint8_t itf, uint8_t buf[], uint32_t count);
 static void cdc_task(void);
 
 void
@@ -16,26 +16,23 @@ usbd_controller_task(void* argument) {
     }
 }
 
-static void echo_serial_port(uint8_t itf, uint8_t buf[], uint32_t count) {
-    uint8_t const case_diff = 'a' - 'A';
-
-    for (uint32_t i = 0; i < count; i++) {
-        tud_cdc_n_write_char(itf, buf[i]);
-    }
-    
-    tud_cdc_n_write_flush(itf);
+// USB CDC
+static void
+cdc_task(void) {
+    tud_cdc_n_write_flush(0);
 }
 
-// USB CDC
-static void cdc_task(void) {
-    uint8_t itf;
+void
+log_write_ll(char* msg) {
+    tud_cdc_n_write(0, msg, strlen(msg));
+}
 
-    for (itf = 0; itf < CFG_TUD_CDC; itf++) {
-        if (tud_cdc_n_available(itf)) {
-            uint8_t buf[64];
-            uint32_t count = tud_cdc_n_read(itf, buf, sizeof(buf));
-            // echo back to both serial ports
-            echo_serial_port(0, buf, count);
-        }
-    }
+void
+USB_HP_CAN1_TX_IRQHandler(void) {
+    return tud_int_handler(0);
+}
+
+void
+USB_LP_CAN1_RX0_IRQHandler(void) {
+    return tud_int_handler(0);
 }
